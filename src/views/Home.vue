5ca45bf6ced7862f67f8286a7e1cc23d74d4aa5b
Surapong <surapong.bird@mail.kmutt.ac.th>
2021-04-14 19:23:16 +0700

    first update

---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+     <h1>The<strong>Cat</strong>Datbase </h1>
+      </header>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  
+}
+</script>
+
+<style language="scss">
+ *{
+     margin:0;
+     padding:0;
+     box-sizing:border-box;
+     font-family:  "Helvetica",sans-serif;
+ }
+ a{
+     text-decoration:none;
+ }
+ header{
+     padding-top:50px;
+     padding-bottom:50px;
+    
+ }  
+ header h1 {
+         color: gray;
+         font-size:42px;
+         text-align:center;
+         text-transform:uppercase;
+     }
+header strong {
+    color: black;
+}
+ 
+
+</style>
